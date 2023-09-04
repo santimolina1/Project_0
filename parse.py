112a31b5fdc806f1604842aef72b233902f36730
SofiaEscobar04 <88900745+SofiaEscobar04@users.noreply.github.com>
2023-09-03 21:37:40 -0500

    parse.py

--- parse.py
+++ parse.py
@@ -1,1 +1,26 @@
 import sys
+
+commands= {'M','R','C','B','c','b','P'}
+directions={'north', 'east', 'west','south'}
+symbols=set([":", ",", ";", "[", "]"])
+orientations={'right','left','front','back'}
+
+variables={}
+functions={}
+pila_calls=[]
+
+
+def convertirPrograma(programa):
+    programa = programa.replace('\t', ' ').replace('\n', ' ')
+    comandos = programa.split()
+    return [comandos.lower() for comando in comandos]
+
+
+def comandoValido(comando):
+    return comando in commands
+
+def direccionValida(comando):
+    return comando in directions
+
+def funcionDefinida(nombre):
+    return nombre in functions
